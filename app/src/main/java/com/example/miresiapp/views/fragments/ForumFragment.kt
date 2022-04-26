9ca996834cc2e7a8ad9a658ad2f9e93f94699aa8
emilio <emiliobotier@gmail.com>
2022-04-26 11:52:14 +0200

    implementing forums

--- app/src/main/java/com/example/miresiapp/views/fragments/ForumFragment.kt
+++ app/src/main/java/com/example/miresiapp/views/fragments/ForumFragment.kt
@@ -6,15 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.miresiapp.R
+import com.example.miresiapp.databinding.FragmentForumBinding
 
 class ForumFragment : Fragment() {
+    private lateinit var binding: FragmentForumBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_forum, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentForumBinding.bind(view)
+
     }
 }
